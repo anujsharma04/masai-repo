594c7f4cc575288292aa68640e13426bdaf1707a
unknown <you@example.com>
2025-03-30 19:48:20 +0530

    store a seprate folder

---
+++ REACT/Component Lifecycle, Side Effects and useEffect Hook/problem2/src/App.js
@@ -0,0 +1,12 @@
+import Item from './component/home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Item/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/Component Lifecycle, Side Effects and useEffect Hook/problem3/src/App.js
@@ -0,0 +1,14 @@
+import Items from "./component/pagenation";
+import Timer from "./component/timer";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Timer/>
+      <Items/>
+    </div>
+  );
+}
+
+export default App;
